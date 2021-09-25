a4fb9cec4562197b83cf222c82c6c1db114717a8
Samanta Fluture <samantafluture@gmail.com>
2021-09-24 22:38:03 -0300

    create card project component

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { LogoComponent } from './sidebar/logo/logo.component';
 import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [SidebarComponent, LogoComponent, HeaderComponent],
+  declarations: [SidebarComponent, LogoComponent, HeaderComponent, CardComponent],
   imports: [CommonModule, AppRoutingModule],
-  exports: [SidebarComponent, LogoComponent, HeaderComponent],
+  exports: [SidebarComponent, LogoComponent, HeaderComponent, CardComponent],
 })
 export class SharedModule {}
