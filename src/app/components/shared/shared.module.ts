03448e6d58edf2dd4165bf900b1bbf3d0ba2cda4
Samanta Fluture <samantafluture@gmail.com>
2021-09-24 21:00:22 -0300

    correct href to follower

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { LogoComponent } from './sidebar/logo/logo.component';
 
 @NgModule({
-  declarations: [SidebarComponent],
-  imports: [CommonModule],
-  exports: [SidebarComponent],
+  declarations: [SidebarComponent, LogoComponent],
+  imports: [CommonModule, AppRoutingModule],
+  exports: [SidebarComponent, LogoComponent],
 })
 export class SharedModule {}
