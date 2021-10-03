47be7bcfc3a1510592b242f0ad6496b90a821471
Samanta Fluture <samantafluture@gmail.com>
2021-10-03 16:40:02 -0300

    fix bug filter sprint by project

--- src/app/components/sprints/sprint-list/sprint-list.component.ts
+++ src/app/components/sprints/sprint-list/sprint-list.component.ts
@@ -1,7 +1,5 @@
-import { ProjectService } from './../../projects/project.service';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { map } from 'rxjs/operators';
-import { Project } from 'src/app/interfaces/project';
 import { Sprint } from 'src/app/interfaces/sprint';
 import {
   isDone,
@@ -11,8 +9,6 @@ import {
   filterByProject,
 } from 'src/app/utils/filter-sprint';
 import { SprintService } from '../sprint.service';
-import { Observable } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sprint-list',
@@ -23,9 +19,6 @@ export class SprintListComponent implements OnInit {
   @Output() clickEvent = new EventEmitter<any>();
   @Input() projectId!: any;
   sprints: Sprint[] = [];
-  // projects: Project[] = [];
-  // id!: number;
-  // project$!: Observable<Project>;
 
   allSprints$ = this.sprintService
     .getSprints()
@@ -49,17 +42,9 @@ export class SprintListComponent implements OnInit {
     map((sprints) => sprints.filter((sprint) => isDone(sprint)))
   );
 
-  // allProjects$ = this.projectService.getProjects();
-
-  constructor(
-    private sprintService: SprintService,
-    // private projectService: ProjectService,
-    // private activatedRoute: ActivatedRoute,
-  ) {}
+  constructor(private sprintService: SprintService) {}
 
   ngOnInit(): void {
-    // this.id = this.activatedRoute.snapshot.params.id;
-    // this.project$ = this.projectService.getProjectById(this.id);
     this.toDoSprints$;
     this.inProgressSprints$;
     this.isDoneSprints$;
