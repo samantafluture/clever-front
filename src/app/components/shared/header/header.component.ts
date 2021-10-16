dadbf52d456ba50e592579bf561f52e8cb1e992c
Samanta Fluture <samantafluture@gmail.com>
2021-10-15 23:16:58 -0300

    refactor css dashboard

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -9,7 +9,7 @@ export class HeaderComponent implements OnInit {
   @Input() pageTitle: string | undefined;
   @Input() btnTitle: string = '';
   @Output() clickEvent = new EventEmitter<any>();
-  isMobile!: boolean;
+  isMobile: boolean = true;
 
   constructor() { }
 
